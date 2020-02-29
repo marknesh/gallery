7641ba08b339a5a1ff62172eb59cefb81155779f
marknesh <markmunene.nesh@gmail.com>
2020-02-29 20:32:46 +0300

    craete test for my model class and assert if it is true

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'mygallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':'images',
         'USER':'mike',
         'PASSWORD':'mark@munene18',
